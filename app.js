0dfe36b215c795e6b2e1faba5fa7857f76a03759
Sandeep Kumar <sandeepkr.799@gmail.com>
2023-11-18 12:59:30 +0530

    nested structure

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello world from react!"
-);
+/*
+ *    <div id="parent">
+ *       <div id="child">
+ *            <h1></h1>
+ *        </div>
+ *    </div>
+ */
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "this is what react is"),
+    React.createElement("h2", {}, "this is h2 react tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "this is what react is"),
+    React.createElement("h2", {}, "this is h2 react tag"),
+  ]),
+]);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world from react!"
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
