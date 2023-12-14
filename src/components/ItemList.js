22e53f69213de97433d098e51b2c801529ab4cdb
Sandeep Kumar <sandeepkr.799@gmail.com>
2023-12-14 08:14:05 +0530

    accordian completed

---
+++ src/components/ItemList.js
@@ -0,0 +1,30 @@
+import { CDN_URL } from "../../utils/constants";
+
+const ItemList = ({ item }) => {
+  //   console.log("item>>>>>>>>>", item);
+  return (
+    <div>
+      {item.map((e) => (
+        <div
+          key={e.card.info.id}
+          className="p-2 m-2 border-gray-300 border-b-2 text-left flex justify-between"
+        >
+          <div className="w-9/12">
+            <div className="py-2">
+              <span>{e.card.info.name}</span>
+              <span>
+                - ₹ {e.card.info.price / 100 || e.card.info.defaultprice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{e.card.info.description}</p>
+          </div>
+          <div className="w-3/12 p-4">
+            <img src={CDN_URL + e.card.info.imageId} className="rounded-2xl" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+
 const RestaurantCategory = (props) => {
-  console.log(props);
+  const [itemCard, setItemCard] = useState(false);
   const { title, itemCards } = props.data;
+  const handleClick = () => {
+    setItemCard(!itemCard);
+  };
   return (
     <div>
-      <div className="w-6/12 mx-auto my-2 bg-gray-50 shadow-lg p-4 flex justify-between">
-        <span>
-          {title} - ({itemCards.length})
-        </span>
-        <span>🔽</span>
+      <div className="w-6/12 mx-auto my-2 bg-gray-50 shadow-lg p-4">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="text-lg font-bold">
+            {title} - ({itemCards.length})
+          </span>
+          <span>🔽</span>
+        </div>
+        {itemCard && <ItemList item={itemCards} />}
       </div>
     </div>
   );
